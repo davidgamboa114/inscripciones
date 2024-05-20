e7a230741768909de3817dafb07468bf6998bdac
davidgamboa114 <davidgamboa114@gmail.com>
2024-05-20 13:09:26 -0300

    Inicio del proyecto

---
+++ Models/InscripcionesContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Inscripciones.Models
+{
+    public class InscripcionesContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;      
+                               Database=InscripcionesContext;
+                               User Id = sa; Password = 123; Encrypt=false");
+        }
+        public virtual DbSet<Alumno> Alumnos { get; set; }
+    }
+
+}

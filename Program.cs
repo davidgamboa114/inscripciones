bab4108013c316b0cae41076d70abb15d3df61b5
davidgamboa114 <davidgamboa114@gmail.com>
2024-05-27 14:26:51 -0300

    habilitando swwager

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
+using Inscripciones.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuracion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<InscripcionesContext>(option =>
+    option.UseSqlServer(configuracion.GetConnectionString("sqlserver")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
